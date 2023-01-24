408f4178cb3aec52c8f3d2e4388ff25e0cb61cfc
wiem assadi <wiemassadi12@gmail.com>
2023-01-24 10:17:49 +0100

    fixed login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthentificationService } from '../services/authentification.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  messageErr =''
+  counter:any
+  dataArray:any = [] ;
+  constructor(private usersService:AuthentificationService,private route:Router  ) {}
+
+  getalluser(){
+    this.usersService.getAllusers().subscribe(data=>{
+      console.log(this.dataArray)
+      this.counter = this.dataArray.length , (err:HttpErrorResponse)=>{
+      this.messageErr="We dont't found this user in our database"} 
+    }) 
+
+  }
 
 }
